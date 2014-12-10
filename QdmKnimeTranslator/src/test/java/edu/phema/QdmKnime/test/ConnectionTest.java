9e970d2afcc788ed41d23eccdf8fe4d6e1f6b369
wkt <thompsow@r1001-lt-100-f.enhnet.org>
2014-12-10 15:08:59 -0600

    added wrapper task and generated gradle wrapper files

--- QdmKnimeTranslator/src/test/java/edu/phema/QdmKnime/test/ConnectionTest.java
+++ QdmKnimeTranslator/src/test/java/edu/phema/QdmKnime/test/ConnectionTest.java
@@ -4,12 +4,16 @@
 package edu.phema.QdmKnime.test;
 
 
+import java.io.StringWriter;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 //import javax.xml.bind.util.JAXBSource;
 //import javax.xml.namespace.QName;
+
+import org.junit.Test;
 
 import edu.phema.QdmKnime.Connection;
 import edu.phema.knime.jaxb.ObjectFactory;
@@ -20,17 +24,8 @@ import edu.phema.knime.jaxb.ObjectFactory;
  */
 public class ConnectionTest {
 
-	/**
-	 * 
-	 */
-	public ConnectionTest() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	@Test
+	public void testConnection() throws JAXBException {
 		// TODO Auto-generated method stub
 		ObjectFactory elementFactory = new ObjectFactory();
 		Connection conn = new Connection();
@@ -41,26 +36,23 @@ public class ConnectionTest {
 		conn.addBendpoint(578, 95);
 		
 		JAXBElement<?> jaxbConn = elementFactory.createConfig(conn.getKnimeWorkflowConfig(elementFactory));
-		try {
 			JAXBContext contextA = JAXBContext.newInstance(ObjectFactory.class);
 			Marshaller m = contextA.createMarshaller();
 			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 			//m.setProperty(Marshaller.JAXB_FRAGMENT, true);
-			m.marshal(jaxbConn, System.out);
 			
-			
+			StringWriter writer = new StringWriter();
+			m.marshal(jaxbConn, writer);
+			String result = writer.toString();
+			assert result != null;
+			System.out.println(result);
 			/*			
 			 * JAXBElement<Config> jaxbElementA = new JAXBElement<Config>(
 					new QName(""), 
 					Config.class, jaxbConn);
 			*/
 			// JAXBSource sourceA = new JAXBSource(contextA, jaxbElementA);
-			
-		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+					
 	}
 
 }
