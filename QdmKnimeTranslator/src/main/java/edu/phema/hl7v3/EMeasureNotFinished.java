8c30b934f37ef9e4771422797b9dbf6fc5999377
henryhmo <huan.mo@vanderbilt.edu>
2015-01-07 18:02:37 -0600

    No change

--- QdmKnimeTranslator/src/main/java/edu/phema/hl7v3/EMeasureNotFinished.java
+++ QdmKnimeTranslator/src/main/java/edu/phema/hl7v3/EMeasureNotFinished.java
@@ -12,8 +12,8 @@ import javax.xml.bind.JAXBException;
 import javax.xml.transform.stream.StreamSource;
 
 import edu.phema.QdmKnime.Toolkit;
-import edu.phema.jaxb.uvqm.hl7v3.POQMMT000001UVQualityMeasureDocument;
-//import edu.phema.jaxb.queryHealth.hqmf.POQMMT000001UVQualityMeasureDocument;
+//import edu.phema.jaxb.uvqm.hl7v3.POQMMT000001UVQualityMeasureDocument;
+import edu.phema.jaxb.queryHealth.hqmf.POQMMT000001UVQualityMeasureDocument;
 
 /**
  * @author moh
@@ -53,7 +53,9 @@ public class EMeasureNotFinished {
 		// TODO Auto-generated method stub
 		String fileDir = 
 				"src/main/resources/hqmf_queryHealth/samples/HQMF_Measure60.xml";
+		String fileDir2 = "src/main/resources/eMeasures/EH_CMS30v2_NQF0639_AMI10_StatinMed/CMS30v2.xml";
 		EMeasureNotFinished em = new EMeasureNotFinished(Toolkit.readFile(fileDir));
+		System.out.println("done");
 	}
 
 }
